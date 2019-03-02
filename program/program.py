adb45c7d9345906be1df9d44fab64b6556576b98
Ruben Barajas <barajas.ruben95@gmail.com>
2019-03-02 10:40:06 -0600

    [FEAT] Agregado el comando para actualizar PIP

--- program/program.py
+++ program/program.py
@@ -1,4 +1,5 @@
 #pip install openpyxl
-#pip install pillow		#To be able to include images (jpeg, png, bmp,…) into an openpyxl file.
+#python -m pip install --upgrade pip		#Actualizar version de pip
+#pip install pillow							#To be able to include images (jpeg, png, bmp,…) into an openpyxl file.
 
 print("Hello Python 2.7.x")
