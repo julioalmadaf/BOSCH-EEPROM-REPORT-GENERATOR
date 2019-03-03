1d4f3d400c4bbe9922f607fc296c0999d9e7eb62
Ruben Barajas <barajas.ruben95@gmail.com>
2019-03-02 20:20:47 -0600

    [FEAT] Open Dialog para seleccionar archivo CNT

--- program/program.py
+++ program/program.py
@@ -2,33 +2,46 @@
 #python -m pip install --upgrade pip		#Actualizar version de pip
 #pip install pillow							#To be able to include images (jpeg, png, bmp) into an openpyxl file.
 #pip install xlrd
-
 #pip install pathlib
 
 import os
 import sys
+import tkinter
+
+from tkinter import messagebox
 from pathlib import Path
 from os import listdir
+
+import tkinter as tk
+from tkinter import filedialog
  
 def list_files(directory, extension):
     return (f for f in listdir(directory) if f.endswith('.' + extension))
 
 def main():
 
-	#Obtener el path actual.
-	mypath = Path().absolute()
-
-	#Buscar archivos .cnt
-	files = list_files(mypath,"cnt")
-	
-	#De cada archivo cnt encontrado, segmentarlo para encontrar el BBNumber y el Baseline.
-	for f in files:
-		BBNumber = f.split('_')[2]
-		Baseline = f.split('_')[3]
+	#Seleccionar container
+	buscarArchivoCNT = 0
+	while buscarArchivoCNT == 0:
 		
+		#Abrir Dialog Box y seleccionar archivo
+		root = tk.Tk()
+		root.withdraw()
+		file_path = filedialog.askopenfilename()
+		print(file_path)
+
+		#Validar que sea un archivo .cnt
+		if(file_path.find(".cnt") != -1):
+			buscarArchivoCNT = 1
+		else:
+			messagebox.showerror("Error", "Not .cnt file selected")
+	
+	#
+	BBNumber = file_path.split('_')[3]
+	Baseline = file_path.split('_')[4]
+	
 	print(BBNumber)
 	print(Baseline)
-	
 
 #################################
 if __name__== "__main__":
