c0b238249834514fe3ce1e529444ee54e41d3444
Ruben Barajas <barajas.ruben95@gmail.com>
2019-03-02 19:04:01 -0600

    [FIX] Caracter especial no podia ser compilado por python (estaba en los comentarios)

--- program/program.py
+++ program/program.py
@@ -1,5 +1,5 @@
 #pip install openpyxl
 #python -m pip install --upgrade pip		#Actualizar version de pip
-#pip install pillow							#To be able to include images (jpeg, png, bmp,…) into an openpyxl file.
+#pip install pillow							#To be able to include images (jpeg, png, bmp) into an openpyxl file.
 
 print("Hello Python 2.7.x")
